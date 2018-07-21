430de2c901479b65a8d1ce3a357d35b75c598ef8
NaKia Whitby <whitby.nakia@gmail.com>
2018-07-21 17:52:52 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,8 @@
 def turn_count(board)
   counter = 0
-  board.each do |token|
-  if token == "X" || token == "O"
+  board.each do |character|
+
+  if character == "X" || character == "O"
     counter +=1
   end
 end
